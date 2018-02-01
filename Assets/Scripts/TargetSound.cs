69cd5e586b82bb8af35d7d1863e8acc2e3f05501
Skiini <skiini03@gmail.com>
2018-02-01 12:27:39 +0100

    Adding sounds to target

---
+++ Assets/Scripts/TargetSound.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetSound : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void PlaySoundTarget()
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
